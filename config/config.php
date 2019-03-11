0b3db90406f98945be3ce3b3a2eee00d07da463f
root <841694874@qq.com>
2019-03-11 18:21:27 +0800

    init

--- config/config.php
+++ config/config.php
@@ -4,16 +4,14 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
-    'id' => 'commands',
-    'basePath' => dirname(__DIR__),
-    'controllerNamespace' => 'app\commands',
+
     'components' => [
         'cache' => [
             'class' => 'min\console\Input',
         ],
         'db' => $db,
     ],
-    'params' => $params,
+    //'params' => $params,
 ];
 
 return $config;
