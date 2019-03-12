d6d547874a171b100f2630afc69086138934eccb
kevin <841694874@qq.com>
2019-03-12 18:52:59 +0800

    init

--- config/config.php
+++ config/config.php
@@ -4,7 +4,18 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
+    // 命令命名空间
+    'commandNamespace' => 'apps\daemon\commands',
 
+    // 命令
+    'commands'         => [
+
+        'push start'   => ['Push', 'Start'],
+        'push stop'    => ['Push', 'Stop'],
+        'push restart' => ['Push', 'Restart'],
+        'push status'  => ['Push', 'Status'],
+
+    ],
     'components' => [
         'cache' => [
             'class' => 'min\console\Input',
