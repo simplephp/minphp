2946c2b60ab3f679409f8258b343c768e2f3f429
root <841694874@qq.com>
2019-03-21 17:56:25 +0800

    ffirst

---
+++ app/commands/TestCommand.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * description
+ * @author         kevin <askyiwang@gmail.com>
+ * @date           2019/3/19
+ * @since          1.0
+ */
+class TestCommand
+{
+
+    public function start() {
+        var_dump('start');
+    }
+}
