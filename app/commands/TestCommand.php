775f88ac86e1af49bc6872983c9925838f80eaa9
kevin <841694874@qq.com>
2019-03-22 18:34:18 +0800

    first

--- app/commands/TestCommand.php
+++ app/commands/TestCommand.php
@@ -11,7 +11,21 @@ namespace app\commands;
 class TestCommand
 {
 
-    public function start() {
-        var_dump('start');
+    public function startAction() {
+        go(function() {
+            $cli = new \Swoole\Coroutine\Http\Client('127.0.0.1', 80);
+            $cli->setHeaders([
+                'Host' => "localhost",
+                "User-Agent" => 'Chrome/49.0.2587.3',
+                'Accept' => 'text/html,application/xhtml+xml,application/xml',
+                'Accept-Encoding' => 'gzip',
+            ]);
+            $cli->set([ 'timeout' => 1]);
+            $cli->get('/index.php');
+
+            $cli->close();
+            return [];
+            yield $cli->body;
+        });
     }
 }
